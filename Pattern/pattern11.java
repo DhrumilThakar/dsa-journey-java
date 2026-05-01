96ea4301ab821850faa2963de86077107fa83a46
Dhrumil Thakar <dhruvmilthakar@gmail.com>
2026-05-01 18:22:59 +0530

    Started solving pattern problem

---
+++ Pattern/pattern11.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class pattern11 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int start=1;
+
+        for(int i =0;i<n;i++){
+            if(i%2==0){
+                start =1;
+            }
+            else{
+                start =0;
+            }
+
+            for(int j =0;j<=i;j++){
+                System.out.print(start);
+                start= 1-start;
+            }
+            System.out.println();
+        }
+        
+    }
+}
