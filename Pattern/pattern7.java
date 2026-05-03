0c40c41be3342eae95767b615a79a115ba085b2c
Dhrumil Thakar <dhruvmilthakar@gmail.com>
2026-05-04 00:09:25 +0530

    Ignore .class files

--- Pattern/pattern7.java
+++ Pattern/pattern7.java
@@ -17,5 +17,17 @@ public class pattern7 {
             }
             System.out.println();
         }
+        for(int i =n;i>=1;i--){
+            for(int j = 1;j<=(n-i);j++){ //Space
+                System.out.print(" ");
+            }
+            for(int k =1;k<=(2*i-1);k++){ //Space
+                System.out.print("*");
+            }
+            for(int j = 1;j<=(n-i);j++){ //Space
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }   
 }
