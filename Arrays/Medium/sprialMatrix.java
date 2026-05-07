e5f6867ffc8242b31425a115818f6966f4c0bae6
Dhrumil Thakar <dhruvmilthakar@gmail.com>
2026-05-07 23:36:35 +0530

    Spiral Traversal of Matrix

---
+++ Arrays/Medium/sprialMatrix.java
@@ -0,0 +1,46 @@
+// Given a Matrix, print the given matrix in spiral order.
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class sprialMatrix {
+     public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+        int top = 0;
+        int bottom =matrix.length-1;
+        int left =0;
+        int right = matrix.length-1;
+        while(left<=right && top<=bottom){
+          for(int i=left;i<=right;i++){
+          list.add(matrix[top][i]);
+        }
+        top++;
+        for(int i=top;i<=bottom;i++){
+          list.add(matrix[i][right]);
+        }
+        right--;
+        if(top<=bottom){
+        for(int i =right;i>=left;i--){
+          list.add(matrix[bottom][i]);
+        }
+        bottom--;
+     }
+     if(left<=right){
+        for(int i = bottom;i>=top;i--){
+          list.add(matrix[i][left]);
+        }
+        left++;
+        }
+     }
+        return list;
+
+    }
+     public static void main(String[] args) {
+          int[][] arr ={ { 1, 2, 3, 4 },{ 5, 6, 7, 8 },{ 9, 10, 11, 12 },{ 13, 14, 15, 16 } };
+
+          List<Integer> result = spiralOrder(arr);
+          for(int i =0;i<result.size();i++){
+               System.out.print(result.get(i)+" ");
+          }
+     }
+}
