6d14d2d11f37595f97505e1be9573d0bc4bdc7f1
Dhrumil Thakar <dhruvmilthakar@gmail.com>
2026-05-08 22:30:27 +0530

    longest palindromic substring

---
+++ String/Medium/longestPalindromic.java
@@ -0,0 +1,36 @@
+//Given a string s, return the longest palindromic substring in s.
+public class longestPalindromic{
+     public static String longestPalindrome(String s) {
+        int n = s.length();
+        String res= "";
+
+        for(int i =0;i<n;i++){
+            int st=i;
+            int end =i;
+            while(st>=0 && end<n && s.charAt(st)==s.charAt(end)){
+                st--;
+                end++;
+            }
+            String temp = s.substring(st+1,end);
+            if(temp.length()>res.length()){
+                res=temp;
+            }
+            st =i;
+            end = i+1;
+            while(st >= 0 && end <n && s.charAt(st)==s.charAt(end)){
+                st--;
+                end++;
+            }
+            temp = s.substring(st+1,end);
+            if(temp.length()>res.length()){
+                res= temp;
+            }
+            
+        }
+        return res;
+    }
+     public static void main(String[] args) {
+          String s = "babad";
+          System.out.println(longestPalindrome(s));
+     }
+}
